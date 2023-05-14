e4a54c6ddea19544045b46b55b04e5ab023f9373
SergioBarretoJr <sergiobarreto@id.uff.br>
2023-05-14 19:24:45 +0000

    week1

--- w1/main.py
+++ w1/main.py
@@ -3,6 +3,8 @@ from w1.data_processor import DataProcessor
 from pprint import pprint
 from typing import Dict
 from tqdm import tqdm
+from collections import defaultdict
+
 import os
 import argparse
 from global_utils import get_file_name, make_dir, plot_sales_data
@@ -45,6 +47,19 @@ def revenue_per_region(dp: DataProcessor) -> Dict:
     """
     ######################################## YOUR CODE HERE ##################################################
 
+    data_gen = (row for row in dp.data_reader)
+    # Skipping the first row as its the column names
+    _ = next(data_gen)
+    # Initializing a default dictionary first
+    rev_agg = defaultdict(lambda: 0)
+    rev_agg = {
+        row["Country"]: rev_agg[row["Country"]]
+        + float(row["UnitPrice"]) * int(row["Quantity"])
+        for row in data_gen
+    }
+
+    return rev_agg
+
     ######################################## YOUR CODE HERE ##################################################
 
 
@@ -53,45 +68,70 @@ def get_sales_information(file_path: str) -> Dict:
     dp = DataProcessor(file_path=file_path)
 
     # print stats
-    dp.describe(column_names=[constants.OutDataColNames.UNIT_PRICE, constants.OutDataColNames.TOTAL_PRICE])
+    dp.describe(
+        column_names=[
+            constants.OutDataColNames.UNIT_PRICE,
+            constants.OutDataColNames.TOTAL_PRICE,
+        ]
+    )
 
     # return total revenue and revenue per region
     return {
-        'total_revenue': dp.aggregate(column_name=constants.OutDataColNames.TOTAL_PRICE),
-        'revenue_per_region': revenue_per_region(dp),
-        'file_name': get_file_name(file_path)
+        "total_revenue": dp.aggregate(
+            column_name=constants.OutDataColNames.TOTAL_PRICE
+        ),
+        "revenue_per_region": revenue_per_region(dp),
+        "file_name": get_file_name(file_path),
     }
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Choose from one of these : [tst|sml|bg]")
-    parser.add_argument('--type',
-                        default='tst',
-                        choices=['tst', 'sml', 'bg'],
-                        help='Type of data to generate')
+    parser = argparse.ArgumentParser(
+        description="Choose from one of these : [tst|sml|bg]"
+    )
+    parser.add_argument(
+        "--type",
+        default="tst",
+        choices=["tst", "sml", "bg"],
+        help="Type of data to generate",
+    )
     args = parser.parse_args()
 
-    data_folder_path = os.path.join(CURRENT_FOLDER_NAME, '..', constants.DATA_FOLDER_NAME, args.type)
-    files = [str(file) for file in os.listdir(data_folder_path) if str(file).endswith('csv')]
+    data_folder_path = os.path.join(
+        CURRENT_FOLDER_NAME, "..", constants.DATA_FOLDER_NAME, args.type
+    )
+    files = [
+        str(file) for file in os.listdir(data_folder_path) if str(file).endswith("csv")
+    ]
 
-    output_save_folder = os.path.join(CURRENT_FOLDER_NAME, '..', 'output', args.type,
-                                      datetime.now().strftime("%B %d %Y %H-%M-%S"))
+    output_save_folder = os.path.join(
+        CURRENT_FOLDER_NAME,
+        "..",
+        "output",
+        args.type,
+        datetime.now().strftime("%B %d %Y %H-%M-%S"),
+    )
     make_dir(output_save_folder)
 
     file_paths = [os.path.join(data_folder_path, file_name) for file_name in files]
-    revenue_data = [get_sales_information(file_path)
-                    for file_path in file_paths]
+    revenue_data = [get_sales_information(file_path) for file_path in file_paths]
 
     pprint(revenue_data)
 
     for yearly_data in revenue_data:
-        with open(os.path.join(output_save_folder, f'{yearly_data["file_name"]}.json'), 'w') as f:
+        with open(
+            os.path.join(output_save_folder, f'{yearly_data["file_name"]}.json'), "w"
+        ) as f:
             f.write(json.dumps(yearly_data))
 
-        plot_sales_data(yearly_revenue=yearly_data['revenue_per_region'], year=yearly_data["file_name"],
-                        plot_save_path=os.path.join(output_save_folder, f'{yearly_data["file_name"]}.png'))
+        plot_sales_data(
+            yearly_revenue=yearly_data["revenue_per_region"],
+            year=yearly_data["file_name"],
+            plot_save_path=os.path.join(
+                output_save_folder, f'{yearly_data["file_name"]}.png'
+            ),
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
